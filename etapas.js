a2b192992de0036c3d61fc99518d0a781edaed7b
Thayrone de Paula Barra <thayronedepaula15@gmail.com>
2021-02-28 11:02:49 -0300

    Initial commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+        {
+            titulo:"VEREADOR",
+            numeros: 5,
+            candidatos:[
+                {
+                    numero:'38111',
+                    nome:"Fulando de Tal",
+                    partido:"ABC",
+                    fotos:[
+                        {url:"38111.png", legenda:"Vereador"}
+                    ]
+                },
+                {
+                    numero:'77222',
+                    nome:"Beltrana da Silva",
+                    partido:"DEFG",
+                    fotos:[
+                        {url:"77222.png", legenda: "Vereador"}
+                    ]
+                }
+            ]
+
+        },
+        {
+            titulo:"PREFEITO",
+            numeros:2,
+            candidatos:[
+                {
+                    numero:'99',
+                    nome:"Ciclano",
+                    partido:"ABC",
+                    Vic:"Cic",
+                    fotos:[
+                        {url:"99.png", legenda:"Prefeito"},
+                        {url:"99_2.png", legenda:"Vice-Prefeito", small:true}
+                    ]
+                },
+                {
+                    numero:'84',
+                    nome:"Zulano",
+                    partido:"QWERTY",
+                    vice:"Zul",
+                    fotos:[
+                        {url:"84.png", legenda:"Prefeito"},
+                        {url:"84_2.png",legenda:"Vice-Prefeito", small:true}
+                    ]
+                }
+            ]
+        }
+]
